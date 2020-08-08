f1d13838146e65fb91287952ef9d6099337d8add
Nishit430 <nksayshello@gmail.com>
2020-08-08 22:03:55 +0530

    Added comments

--- Script/Recognition.R
+++ Script/Recognition.R
@@ -1,44 +1,55 @@
 library("ggplot2")
 library(viridis)
+library(dplyr)
+library(nnet)
 set.seed(18031)
 
 #Reading the data
-rfmm <- read.csv("C:\\Users\\nksay\\Documents\\Recognition\\Data\\Reward Received to Analytics.csv")
+rfmm <- read.csv("./Data/Reward Received to Analytics.csv")
 head(rfmm)
-rfmm$Effort <- as.numeric(as.character((rfmm$Effort)))
-rfmm$Support <- as.numeric(as.character((rfmm$Support)))
-rfmm$Feedback <- as.numeric(as.character((rfmm$Feedback)))
-rfmm$X1.on.1 <- as.numeric(as.character((rfmm$X1.on.1)))
-rfmm <-na.omit(rfmm)
+str(rfmm)
+rfmm$No..of.Times.Nominated <- as.numeric(rfmm$No..of.Times.Nominated)
+colSums(is.na(rfmm))
 aa <- rfmm
 str(aa)
-head(rfmm)
 
 ## Scaling data
-rfmm$Effort <- scale(rfmm$Effort)
-rfmm$Support <- scale(rfmm$Support)
-rfmm$Feedback <- scale(rfmm$Feedback)
-rfmm$X1.on.1 <- scale(rfmm$X1.on.1)
+rfmm <- rapply(rfmm, scale, class = c("numeric", "integer"), how = "replace")
+str(rfmm)
 
 ## Finding the optimum number of clusters
-nocl <- sum((nrow(rfmm[,c(-1,-3,-4,-5)])-1)*var(rfmm[,c(-1,-3,-4,-5)]))
+nocl <- sum((nrow(rfmm[,c(-1,-6)])-1)*var(rfmm[,c(-1,-6)]))
 for (i in 2:10) {
-  nocl[i] <- kmeans(rfmm[,c(-1,-3,-4,-5)], centers = i, iter.max = 1000)$tot.withinss
+  nocl[i] <- kmeans(rfmm[,c(-1,-6)], centers = i, iter.max = 1000)$tot.withinss
 }
 nocl
 plot(1:10, nocl, type="b", xlab = "no. of clusters", ylab = "tot within")
 
-# We can take 4 to 6 cluster. Let's take 4 first and then try with5 and 6
-ak <- kmeans(rfmm[,c(-1,-3,-4,-5)], centers = 6, iter.max = 1000, nstart = 25)
+# 3 Clusters seem appropriate
+ak <- kmeans(rfmm[,c(-1,-6)], centers = 3, iter.max = 1000, nstart = 25)
 a1 <- cbind(aa, ak$cluster)
-head(a1)
+str(a1)
 
+# Box plot showing nominations vs efforts. There doesn't seem to be any correlation. But let's check the  cluster wise
+ggplot(aa, aes(y= Effort, x = as.factor(No..of.Times.Nominated), fill = as.factor(a1$No..of.Times.Nominated))) + geom_boxplot() + xlab("No. of nominations") + scale_fill_discrete(name = "")
 # Boxplot showing the distribution of the cluster
 ggplot(aa, aes(y= Effort, x = as.factor(No..of.Times.Nominated), fill = as.factor(a1$`ak$cluster`))) + geom_boxplot() + xlab("No. of nominations") + scale_fill_discrete(name = "Cluster")
 
-# Another visual representation of the clusters this time more clearly 
-ggplot(aa, aes(y= Effort, x = No..of.Times.Nominated, col = as.factor(a1$`ak$cluster`))) + geom_point()+ xlab("No. of nominations") + scale_color_discrete(name = "Cluster")
 
+# Checking if no .of nominations for clusters are different or not
+tbl = table(a1$`ak$cluster`, a1$No..of.Times.Nominated)
+tbl
+chisq.test(tbl)
+# There is no support that more usage of thanks can actually increase managerial effectiveness
+
+# Another way to test using multinomal regression
+# a1$`ak$cluster` <- relevel(as.factor(a1$`ak$cluster`), ref = "3")
+# test <- multinom(a1$`ak$cluster` ~ a1$No..of.Times.Nominated)
+# summary(test)
+# z <- summary(test)$coefficients/summary(test)$standard.errors
+# z
+# p <- (1 - pnorm(abs(z), 0, 1))*2
+# p # all p values are insignificant
 
 clus1 <- subset(a1, a1$"ak$cluster"==1) ## This is read as subset of a1 where a1$column name is equal to 1
 summary(clus1)## This is cluster 1 of people who have have never been nominated and still have a mid level score of manager recognition
@@ -52,9 +63,5 @@ clus3 <- subset(a1, a1$`ak$cluster` == 3)
 summary(clus3)
 nrow(clus3)
 
-clus4 <- subset(a1, a1$`ak$cluster` == 4)
-summary(clus4)
-nrow(clus4)
-
 # Writing this to a csv
-write.csv(a1, "C:\\Users\\nksay\\Documents\\Recognition\\Output\\kmeans6.csv")
+write.csv(a1, "./Output/kmeans4.csv")
